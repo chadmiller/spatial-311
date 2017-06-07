e81b55d893a526ccbefc812f019957e294f61f0c
chadmiller <chad.miller@canonical.com>
2017-06-06 20:03:48 -0400

    Initial import

--- syp/views.py
+++ syp/views.py
@@ -16,5 +16,5 @@ def api_get_point_info(request):
 
     with psycopg2.connect("dbname='{}'".format(db_name)) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT topic.name, parcel_tags.id, parcel_tags.key, parcel_tags.value, parcel_tags.make_visible FROM parcel_tags LEFT JOIN parcel on (parcel_tags.parcel=parcel.id) LEFT JOIN import ON (parcel.import=import.id) LEFT JOIN topic on (topic.id=import.topic) WHERE ST_Contains(parcel.shape, ST_MakePoint({},{})) ORDER BY topic.name, parcel_tags.ordering, parcel_tags.key;".format(lng, lat))
+            cur.execute("SELECT topic.name, parcel_tags.id, parcel_tags.key, parcel_tags.value, parcel_tags.make_visible AND parcel_tags.value IS NOT NULL FROM parcel_tags LEFT JOIN parcel on (parcel_tags.parcel=parcel.id) LEFT JOIN import ON (parcel.import=import.id) LEFT JOIN topic on (topic.id=import.topic) WHERE ST_Contains(parcel.shape, ST_MakePoint({},{})) ORDER BY topic.name, parcel.id, parcel_tags.ordering, parcel_tags.key;".format(lng, lat))
             return JsonResponse({ "data": [(k, list(v)) for k, v in groupby(cur.fetchall(), key=lambda trio: trio[0])] })
